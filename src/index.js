141bde941beec46c184b64f82169abdf5bec86ff
akhfirdaus07 <121778146+akhfirdaus07@users.noreply.github.com>
2023-02-22 10:50:07 +0700

    ADdd bg image

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
 import './style.css';
+import bgImageSrc from './bg-image.png';
+
 
 
 function component(){
+    const bgImage = new Image();
+    bgImage.src = bgImageSrc;
+
     const createH1=document.createElement('h1');
     const createImg=document.createElement('img');
-    const createP=document.createElement('p');
+    const createH4=document.createElement('h4');
     const content=document.querySelector('#content');
 
     createH1.innerHTML="Nusantara Restaurant";
     createImg.src="https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cmVzdGF1cmFudHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=700&q=60";
-    createP.innerHTML="The best restaurant in Indonesia!";
+    createH4.innerHTML="The best restaurant in Indonesia!";
 
     content.appendChild(createH1)
     content.appendChild(createImg)
-    content.appendChild(createP)
+    content.appendChild(createH4)
 }
 
 component();
